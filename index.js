8748a8972ba3e408962a55cfa1eb440449922667
Yoel Yukalo <yoelyukalo@gmail.com>
2020-11-21 16:16:05 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let msgArray = []
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+      msgArray.push(message)
+    }
+    return msgArray
+  }
+  
+  function countDown(num) {
+    let i = 0 
+    while (i <= num) {
+      console.log(num-i)
+      i++
+    }
+  }
