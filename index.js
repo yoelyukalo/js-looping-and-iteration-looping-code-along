f45b5829c3a1f1a5ac5300ebbe8a0d71f32423eb
yoelyukalo <yoelyukalo@gmail.com>
2021-01-18 17:38:41 -0600

    commit tree 0665b28d9e64734fee0fd5f934061f3fd4557474

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 function writeCards(names, event) {
-    let msgArray = []
-    for (let i = 0; i < names.length; i++) {
-      let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
-      msgArray.push(message)
-    }
-    return msgArray
-  }
+  //   let msgArray = []
+  //   for (let i = 0; i < names.length; i++) {
+  //     let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+  //     msgArray.push(message)
+  //   }
+  //   return msgArray
+  // }
   
-  function countDown(num) {
-    let i = 0 
-    while (i <= num) {
-      console.log(num-i)
-      i++
-    }
+  // function countDown(num) {
+  //   let i = 0 
+  //   while (i <= num) {
+  //     console.log(num-i)
+  //     i++
+  //   }
+
+  
   }
